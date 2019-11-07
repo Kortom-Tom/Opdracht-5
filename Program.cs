8c7bdfeb0ac935b1472586d0043ce930f068c7e0
Kortom-Tom <55277950+Kortom-Tom@users.noreply.github.com>
2019-11-07 14:23:40 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Opdracht_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1900; i < 2020; i++)
+
+            Console.WriteLine("{0} is schrikkeljaar: {1}", i, DateTime.IsLeapYear(i));
+            Console.ReadKey();
+
+        }
+
+    }
+}
